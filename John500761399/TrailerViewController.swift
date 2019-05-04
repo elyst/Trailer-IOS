f05c729436ce311a5b130e9ae4486af9fa132ea1
John Verdonck <johnverdonck@hotmail.com>
2019-05-04 18:21:08 +0200

    added trailer share button

---
+++ John500761399/TrailerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TrailerViewController.swift
+//  John500761399
+//
+//  Created by John Verdonck on 02/05/2019.
+//  Copyright © 2019 John Verdonck. All rights reserved.
+//
+
+import UIKit
+
+class TrailerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
